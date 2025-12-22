94fe768e673e0c3f15fac2923ea4e245c938a516
chaewookim <chaewookim2877@gmail.com>
2025-12-22 16:24:57 +0900

    [feat] #12 Brand 정보 출력 테스트 코드 작성

--- src/main/java/com/burger/burgerking/story/application/BrandService.java
+++ src/main/java/com/burger/burgerking/story/application/BrandService.java
@@ -2,12 +2,9 @@ package com.burger.burgerking.story.application;
 
 import com.burger.burgerking.global.error.CustomException;
 import com.burger.burgerking.global.error.ErrorCode;
-import com.burger.burgerking.story.dao.BrandRepository;
 import com.burger.burgerking.story.domain.FileMetaData;
 import com.burger.burgerking.story.dto.FileMetaDataRepository;
-import com.burger.burgerking.story.dto.response.BrandResponse;
 import com.burger.burgerking.story.dto.response.FileMetaDataResponse;
-import com.burger.burgerking.story.dto.response.wrapper.BrandAndFileMetaDataResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -17,7 +14,6 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class BrandService {
-    private final BrandRepository brandRepository;
     private final FileMetaDataRepository fileMetaDataRepository;
 
     public List<FileMetaDataResponse> getInfo() {
