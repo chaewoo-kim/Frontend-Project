b94d59a87cd7b7a3096f3bca17c3eec881e20ce7
danielhw99 <danielhw99@gmail.com>
2025-12-26 15:22:34 +0900

    [fix] #18 MainPage redirect/forward 주소값 수정

--- src/main/java/com/burger/burgerking/mainpage/api/MainpageController.java
+++ src/main/java/com/burger/burgerking/mainpage/api/MainpageController.java
@@ -43,11 +43,7 @@ public class MainpageController {
         return ResponseEntity.ok(ApiResponse.success(response));
     }
 
-    @Operation(summary = "광고영상-더보기 링크", description = "'더보기'란을 통해 광고영상 페이지로 Forward이동합니다.")
-    @GetMapping("/morevideos")
-    public ResponseEntity<ApiResponse<List<AdVideoRequest>>> morevideos(){
-        return ResponseEntity.ok(ApiResponse.success(null));
-    }
+
 
 /*
 *   Redirect Links
@@ -61,13 +57,19 @@ public class MainpageController {
     @Operation(summary = "버거킹 배달앱 플레이스토어 링크", description = "버거킹 배달앱링크로 redirect 이동합니다.")
     @GetMapping("/playstore")
     public String playstore(){
-        return "redirect:/https://play.google.com/store/apps/details?id=kr.co.burgerkinghybrid";
+        return "redirect:https://play.google.com/store/apps/details?id=kr.co.burgerkinghybrid";
+    }
+
+    @Operation(summary = "광고영상-더보기 링크", description = "'더보기'란을 통해 광고영상 페이지로 Forward이동합니다.")
+    @GetMapping("/morevideos")
+    public String morevideos(){
+        return "story/adarchive";
     }
 
     @Operation(summary = "버거킹 배달앱 앱스토어 링크", description = "버거킹 배달앱링크로 redirect 이동합니다.")
     @GetMapping("/appstore")
     public String appstore(){
-        return "redirect:/https://apps.apple.com/kr/app/%EB%B2%84%EA%B1%B0%ED%82%B9-%ED%96%84%EB%B2%84%EA%B1%B0-%ED%82%B9%EC%98%A4%EB%8D%94-%EB%94%9C%EB%A6%AC%EB%B2%84%EB%A6%AC/id1017567032";
+        return "redirect:https://apps.apple.com/kr/app/%EB%B2%84%EA%B1%B0%ED%82%B9-%ED%96%84%EB%B2%84%EA%B1%B0-%ED%82%B9%EC%98%A4%EB%8D%94-%EB%94%9C%EB%A6%AC%EB%B2%84%EB%A6%AC/id1017567032";
     }
 
 /*
@@ -81,19 +83,19 @@ public class MainpageController {
             TODO 링크확인 1
             리턴 링크 다시 확인해야함. "./" 이 문법이 맞는가?
         */
-        return "./menu/main";
+        return "menu/main";
     }
 
     @Operation(summary = "매장소개 링크", description = "메인페이지 매장소개 파트:매장소개링크 이동(Forward)")
     @GetMapping("/store")
     public String store(){
-        return "./store/main";
+        return "store/main";
     }
 
     @Operation(summary = "why버거킹 링크", description = "why버커깅 링크로 Forward이동")
     @GetMapping("/why")
     public String whyburgerking(){
-        return "./story/why";
+        return "story/why";
     }
 
     // 배달링크(이미지)
@@ -104,13 +106,13 @@ public class MainpageController {
             TODO 링크확인 2
             리턴 링크 다시 확인해야함. "/call" 을 통해 전화를 걸도록 동작시킬 수 있는가?
         */
-        return "redirect:/call";
+        return "redirect:call";
     }
 
     @Operation(summary = "브랜드-스토리 링크", description = "스토리의 브랜드 링크를 통해 Forward 이동")
     @GetMapping("/brand")
     public String brand(){
-        return "./story/brand";
+        return "story/brand";
     }
 
 }
