00e0f81084c9c6d2d5398ebeca9c6b22e5ecbb96
danielhw99 <danielhw99@gmail.com>
2025-12-29 12:43:55 +0900

    [feat] #19 고객 지원 API 구현

--- src/main/java/com/burger/burgerking/customer/api/QalistController.java
+++ src/main/java/com/burger/burgerking/customer/api/QalistController.java
@@ -1,9 +1,11 @@
 package com.burger.burgerking.customer.api;
 
+import com.burger.burgerking.customer.application.CustomerService;
 import com.burger.burgerking.customer.dto.response.QaSummaryResponse;
 import com.burger.burgerking.global.common.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,14 +13,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Tag(name = "문의사항", description ="문의사항 세션" )
+@Tag(name = "문의사항 목록", description ="문의사항 목록을 카테고리별로 모아서 조회" )
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/v1/customer/qalist")
 public class QalistController {
 
-    @Operation(summary = "문의사항 조회 기능 제공", description = "모든 문의사항을 조회")
+    private final CustomerService customerService;
+
+    @Operation(summary = "전체 목록", description = "전체 목록 조회")
     @GetMapping
-    public ResponseEntity<ApiResponse<List<QaSummaryResponse>>> getQalist() {
-        return ResponseEntity.ok(ApiResponse.success(null));
+    public ResponseEntity<ApiResponse<List<QaSummaryResponse>>> getAll() {
+        return ResponseEntity.ok(ApiResponse.success(customerService.getAllQa()));
     }
+
+    //카테고리별 조회
 }
