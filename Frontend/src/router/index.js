1d783f22e7f44bb9ad012379797de8eeefa5507e
danielhw99 <danielhw99@gmail.com>
2026-01-02 11:49:47 +0900

    [feat] #35 고객지원 Front common 수정사항
    - package.jon
    - package-lock.json
    - src/router/index.js

--- Frontend/src/router/index.js
+++ Frontend/src/router/index.js
@@ -1,12 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { mainRoutes } from "@/features/main/router.js";
 import { menuRoutes } from "@/features/menu/router.js";
+import { customerRoutes } from "@/features/customer/router.js";
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         ...mainRoutes,
-        ...menuRoutes
+        ...menuRoutes,
+        ...customerRoutes
     ],
     scrollBehavior(to, from, savedPosition) {
         if (savedPosition) {
