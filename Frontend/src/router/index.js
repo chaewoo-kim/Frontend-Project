0b37afea26b7281b4de5f2b39f4158e96923ebca
chaewookim <chaewookim2877@gmail.com>
2026-01-02 11:48:39 +0900

    [feat] #23 Story 프론트엔드 구현

--- Frontend/src/router/index.js
+++ Frontend/src/router/index.js
@@ -1,12 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { mainRoutes } from "@/features/main/router.js";
 import { menuRoutes } from "@/features/menu/router.js";
+import { storyRoutes } from "@/features/story/router.js";
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         ...mainRoutes,
-        ...menuRoutes
+        ...menuRoutes,
+        ...storyRoutes
     ],
     scrollBehavior(to, from, savedPosition) {
         if (savedPosition) {
