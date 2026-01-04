237eca645428516fffdeb53eb9c554ecef758022
danielhw99 <danielhw99@gmail.com>
2026-01-05 02:09:32 +0900

    [feat] #45 메인페이지 Front 구현

--- Frontend/src/features/main/views/MainView.vue
+++ Frontend/src/features/main/views/MainView.vue
@@ -17,8 +17,8 @@
                 :pagination="{ clickable: true }"
                 class="mainSwiper"
               >
-                <swiper-slide 
-                  v-for="banner in eventImages" 
+                <swiper-slide
+                  v-for="banner in eventImages"
                   :key="banner.fileUrl"
                   style="cursor: pointer"
                   @click="handleLink('/news-event')"
@@ -80,9 +80,9 @@
               </div>
               <div class="media_swiper">
                 <div class="video_list">
-                  <div 
-                    v-for="video in storyAdVideos" 
-                    :key="video.id" 
+                  <div
+                    v-for="video in storyAdVideos"
+                    :key="video.id"
                     class="video_item"
                     @click="handleExternalLink(video.fileUrl)"
                   >
@@ -106,16 +106,21 @@
               <ul>
                 <li>
                   <div class="card01">
-                    <div class="card_info"><div class="txt_chip01"><span>메뉴소개</span></div></div>
+                    <div class="card_info">
+                      <div class="txt_chip01"><span>메뉴소개</span></div>
+                    </div>
                     <h3 class="tit_card"><span>버거킹만이 드릴 수 있는 특별한 맛을 소개합니다</span></h3>
                     <div class="card_cont">
                       <div class="r_btn">
-                        <button type="button" class="btn_move" @click="handleLink(mainData.links?.menu)">
+                        <button type="button" class="btn_move" @click="handleLink('/menu')">
                           <span>메뉴 둘러보기</span>
+                          <img src="@/assets/img/ico_bt_link.svg" alt="" class="ico_link">
                         </button>
                       </div>
                     </div>
-                    <div class="image_area"><img src="https://mob-prd.burgerking.co.kr/images/story/main/2025/01/20/fb8997c7-170d-484c-a48b-fc5e1abe97ba.png" alt="Menu"></div>
+                    <div class="image_area image_size01">
+                      <img src="https://mob-prd.burgerking.co.kr/images/story/main/2025/01/20/fb8997c7-170d-484c-a48b-fc5e1abe97ba.png" alt="Menu Intro">
+                    </div>
                   </div>
                 </li>
                 <li>
@@ -124,8 +129,9 @@
                     <h3 class="tit_card"><span>가장 맛있는 버거를 제공하는 프리미엄 QSR 브랜드 매장</span></h3>
                     <div class="card_cont">
                       <div class="r_btn">
-                        <button type="button" class="btn_move" @click="handleLink(mainData.links?.store)">
+                        <button type="button" class="btn_move" @click="handleLink('/store/search')">
                           <span>가까운 매장 찾아보기</span>
+                          <img src="@/assets/img/ico_bt_link.svg" alt="" class="ico_link">
                         </button>
                       </div>
                     </div>
@@ -133,26 +139,19 @@
                   </div>
                 </li>
                 <li>
-                  <div class="card01 pd0">
-                    <swiper
-                      :modules="[Autoplay, Pagination]"
-                      :slides-per-view="1"
-                      :loop="true"
-                      :autoplay="{ delay: 4000 }"
-                      :pagination="{ clickable: true }"
-                    >
-                      <swiper-slide v-for="slide in mainData.whyBkSlides" :key="slide.id">
-                        <div class="card_cont">
-                          <h3 class="tit_card" v-html="slide.title + '<br>' + slide.description"></h3>
-                          <div class="r_btn">
-                            <button type="button" class="btn_move" @click="handleLink(mainData.links?.whyBk)">
-                              <span>Why 버거킹</span>
-                            </button>
-                          </div>
-                        </div>
-                        <div class="image_area"><img :src="slide.imageUrl" :alt="slide.title"></div>
-                      </swiper-slide>
-                    </swiper>
+                  <div class="card01">
+                    <h3 class="tit_card" style="text-align: center; margin-bottom: 20px;">맛에 대한 신뢰<br>Trust in Taste</h3>
+                    <div class="card_cont" style="display: flex; justify-content: flex-end; padding-bottom: 0;">
+                      <div class="r_btn">
+                        <button type="button" class="btn_move" @click="handleLink('/story/why')" style="gap: 5px;">
+                          <span>Why 버거킹</span>
+                          <img src="@/assets/img/ico_bt_link.svg" alt="" class="ico_link">
+                        </button>
+                      </div>
+                    </div>
+                    <div class="image_area image_size01" style="margin-top: auto;">
+                      <img src="https://mob-prd.burgerking.co.kr/images/story/web/2025/01/11/c2a57504-636d-49b9-a7c4-5bb1dda82ac8.png" alt="Trust in Taste">
+                    </div>
                   </div>
                 </li>
               </ul>
@@ -163,14 +162,13 @@
           <div class="home_cont">
             <div class="web_container">
               <div class="delivery_banner" @click="handleExternalLink(mainData.links?.order)">
-                <div class="cont">
-                  <div class="title">
-                    <h2 class="tit_home">집에서 만나는 버거킹</h2>
-                    <p>딜리버리 전화주문 안내 09:00 - 19:00 / 연중무휴<br><span class="txt_sub">(매장별로 상이할 수 있습니다.)</span></p>
-                  </div>
+                <div class="title_area">
+                  <h2 class="tit_home">집에서 만나는 버거킹</h2>
+                  <p>딜리버리 전화주문 안내 09:00 - 19:00 / 연중무휴<br><span class="txt_sub">(매장별로 상이할 수 있습니다.)</span></p>
+                </div>
+                <div class="tel_area">
                   <div class="tel"><strong>1599 - 0505</strong></div>
                 </div>
-                <button type="button" class="btn_detail"><span>detail</span></button>
               </div>
             </div>
           </div>
@@ -181,7 +179,7 @@
               <div class="title"><h2 class="tit_home">고객과 함께 성장하는 버거킹</h2></div>
               <div class="brand_cont">
                 <div class="brand01">
-                  <div class="card01" @click="handleLink(mainData.links?.brand)">
+                  <div class="card01" @click="handleLink('/story/brand')">
                     <div class="card_head"><span class="txt_chip01">브랜드</span></div>
                     <div class="card_cont"><h3 class="tit_card">Since 1954, 전 세계 고객에게 사랑받는 글로벌 브랜드 버거킹!</h3></div>
                   </div>
@@ -218,13 +216,25 @@
                   </div>
                 </div>
                 <div class="cont">
-                  <h2 class="tit"><strong><em>오직 버거킹 앱에서</em>만 가능한 <em>혜택</em> <br>지금 앱 다운로드 받으시고 혜택을 누리세요!</strong></h2>
-                  <div class="benefit_list"><strong>픽업오더</strong><strong>딜리버리 오더</strong><strong>멤버십 적립</strong><strong>할인쿠폰</strong></div>
+                  <h2 class="tit">
+                    <span class="txt_red">오직 버거킹 앱에서만 가능한 혜택</span>
+                    <span class="txt_brown">지금 앱 다운로드 받으시고 혜택을 누리세요!</span>
+                  </h2>
+                  <div class="benefit_list">
+                    <strong>픽업오더</strong>
+                    <strong>딜리버리 오더</strong>
+                    <strong>멤버십 적립</strong>
+                    <strong>할인쿠폰</strong>
+                  </div>
                   <div class="app_down">
-                    <div class="QR"></div>
+
                     <div class="btn_area">
-                      <button type="button" class="btn_app aos" @click="handleExternalLink(mainData.links?.playStore)"><span>Google Play에서 다운로드</span></button>
-                      <button type="button" class="btn_app ios" @click="handleExternalLink(mainData.links?.appStore)"><span>App Store에서 다운로드</span></button>
+                      <button type="button" class="btn_app image_btn" @click="handleExternalLink('https://play.google.com/store/apps/details?id=kr.co.burgerking')">
+                        <img src="@/assets/img/btn_down_aos.svg" alt="Google Play에서 다운로드">
+                      </button>
+                      <button type="button" class="btn_app image_btn" @click="handleExternalLink('https://apps.apple.com/kr/app/id1095254627')">
+                        <img src="@/assets/img/btn_down_ios.svg" alt="App Store에서 다운로드">
+                      </button>
                     </div>
                   </div>
                 </div>
@@ -369,6 +379,7 @@ onMounted(() => {
   border-radius: 20px;
   overflow: hidden;
   box-shadow: 0 10px 30px rgba(0,0,0,0.1);
+  margin-top: 25px;
 }
 
 /* Swiper Slide Styles */
@@ -421,7 +432,7 @@ onMounted(() => {
   overflow: hidden;
   box-shadow: 0 4px 15px rgba(0,0,0,0.05); /* Keeping shadow as it looks nice, though not explicitly in snippet it was there before */
   transition: transform 0.3s;
-  cursor: pointer;
+  cursor: default;
 }
 
 @media screen and (min-width: 640px) {
@@ -493,7 +504,7 @@ onMounted(() => {
   .home_app_down_card {
     padding: 30px;
   }
-  
+
   .down_store {
     display: block;
   }
@@ -528,7 +539,7 @@ onMounted(() => {
     border-radius: 5px;
     overflow: hidden;
   }
-  
+
   .QR img {
       width: 100%;
       height: 100%;
@@ -581,7 +592,7 @@ onMounted(() => {
   .top_cont .card01:not(:first-child) {
     margin: 20px 0 0;
   }
-  
+
   .top_cont .card01 {
     font-size: 1.875rem;
     padding: 30px;
@@ -663,7 +674,7 @@ onMounted(() => {
 @media screen and (min-width: 1500px) {
   .home_search {
     /* Resetting background image logic if simpler style preferred, or keeping it if compatible */
-    background-image: none; 
+    background-image: none;
   }
 }
 
@@ -782,30 +793,176 @@ onMounted(() => {
   padding: 0;
 }
 
+.home_intro ul li {
+  display: flex;
+  flex-direction: column;
+  min-width: 0;
+}
+
+.home_intro ul li .card01 {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  /* width: 100%; explicit width not strictly needed with flex column but okay */
+}
+
+.home_intro .card01 .image_area {
+  margin: 20px -20px -20px;
+  width: calc(100% + 40px);
+  border-radius: 0 0 10px 10px;
+}
+
+@media screen and (min-width: 640px) {
+  .home_intro .card01 .image_area {
+    margin: 30px -30px -30px;
+    width: calc(100% + 60px);
+    border-radius: 0 0 20px 20px;
+  }
+}
+
+@media screen and (min-width: 1024px) {
+  .home_intro .card01 .image_area {
+    margin: 40px -40px -40px;
+    width: calc(100% + 80px);
+  }
+}
+
 .txt_chip01 {
-  background: #F5EBDC;
-  color: #502314;
-  padding: 5px 15px;
-  border-radius: 20px;
-  font-weight: bold;
-  font-size: 0.8rem;
   display: inline-block;
-  margin-bottom: 15px;
+  text-align: center;
+  white-space: nowrap;
+  min-width: 42px;
+  padding: 5px 7px 4px;
+  font-size: .625rem;
+  line-height: 1.2;
+  background: #faf4ed;
+  border-radius: 8px;
+  color: #502314;
+  font-weight: 800;
+}
+
+@media screen and (min-width: 640px) {
+  .txt_chip01 {
+    min-width: 80px;
+    height: 36px;
+    padding: 10px 16px;
+    font-size: .8125rem;
+    line-height: 16px;
+  }
+}
+
+@media screen and (min-width: 1024px) {
+  .txt_chip01 {
+    min-width: 84px;
+    height: 36px;
+    padding: 10px 16px;
+    font-size: .875rem;
+    line-height: 16px;
+  }
 }
 
 .tit_card {
-  font-size: 1.25rem;
+  font-family: var(--font-BKR);
+  color: var(--normal);
+  font-size: 1.0625rem;
+  line-height: 150%;
+  margin-top: 10px;
+}
+
+.home_intro .tit_card {
+  font-size: 0.7rem; /* ~40% reduction from 1.0625rem */
   font-weight: 900;
-  line-height: 1.4;
-  margin-bottom: 20px;
+}
+
+@media screen and (min-width: 640px) {
+  .tit_card {
+    font-size: 1.375rem;
+    line-height: 1.1;
+    margin-top: 18px;
+  }
+  .home_intro .tit_card {
+    font-size: 0.825rem; /* ~40% reduction from 1.375rem */
+  }
+}
+
+@media screen and (min-width: 1024px) {
+  .tit_card {
+    font-size: 2.25rem;
+    line-height: 1.2;
+    margin-top: 18px;
+    flex: 1;
+  }
+  .home_intro .tit_card {
+    font-size: 1.35rem; /* 40% reduction from 2.25rem */
+  }
 }
 
 .btn_move {
-  background: #E2221F;
-  color: white;
-  padding: 10px 25px;
-  border-radius: 25px;
-  font-weight: bold;
+  position: relative;
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  min-height: 18px;
+  padding: 0 10px;
+  font-size: .9375rem;
+  color: var(--normal);
+  font-family: var(--font-BKR);
+  font-weight: 800;
+  border: none;
+  cursor: pointer;
+}
+
+.btn_move:hover {
+  text-decoration: underline;
+}
+
+.ico_link {
+  width: 18px;
+  height: auto;
+}
+
+.btn_move:before {
+  content: "";
+  position: absolute;
+  left: -5px;
+  top: -5px;
+  width: calc(100% + 10px);
+  height: calc(100% + 10px);
+}
+
+@media screen and (min-width: 640px) {
+  .btn_move {
+    min-height: 24px;
+    font-size: 1.0625rem;
+    gap: 15px;
+  }
+  .ico_link {
+    width: 24px;
+  }
+}
+
+@media screen and (min-width: 1024px) {
+  .btn_move {
+    max-width: fit-content;
+    height: 34px;
+    padding: 0 20px;
+    font-size: 1.0625rem;
+    gap: 20px;
+  }
+  .ico_link {
+    width: 24px;
+  }
+}
+
+.r_btn {
+  display: flex;
+  justify-content: flex-end;
+}
+
+.card_cont {
+  flex: 1;
+  margin-top: 18px;
+  position: relative;
 }
 
 /* Delivery Banner */
@@ -823,17 +980,31 @@ onMounted(() => {
 .delivery_banner .tit_home {
   color: white;
   margin-bottom: 10px;
+  font-family: var(--font-BKR); /* Or specific bold font */
+  font-size: 2.5rem;
+}
+
+.delivery_banner p {
+  font-size: 1rem;
+  opacity: 0.8;
+}
+
+.tel_area {
+  display: flex;
+  align-items: center;
 }
 
 .tel strong {
-  font-size: 3rem;
+  font-size: 3.5rem;
   font-family: var(--font-flame);
+  color: #f2a900; /* Matching BK yellow */
 }
 
 /* Brand Section */
+/* Brand Section */
 .brand_cont {
-  display: grid;
-  grid-template-columns: 1fr 1fr;
+  display: flex;
+  flex-direction: column;
   gap: 20px;
 }
 
@@ -843,64 +1014,216 @@ onMounted(() => {
   gap: 20px;
 }
 
+@media screen and (min-width: 640px) {
+  .brand_cont {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 20px;
+  }
+}
+
+@media screen and (min-width: 1024px) {
+  .brand_cont {
+    grid-template-columns: repeat(3, 1fr);
+  }
+}
+
+.brand_cont .tit_card {
+  font-size: 1.05rem; /* Increased by 20% from 0.875rem */
+  font-weight: 900;
+  margin-top: 10px;
+}
+
+.brand_cont .card01 {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+}
+
+.brand_cont .card01 .card_cont {
+  flex: 1; /* Pushes content/image apart if needed, or just let it flow */
+}
+
+/* Full bleed image for brand section */
+.brand_cont .card01 .image_area {
+  margin: 20px -20px -20px;
+  width: calc(100% + 40px);
+  border-radius: 0 0 10px 10px;
+  margin-top: auto; /* Push to bottom */
+}
+
+@media screen and (min-width: 640px) {
+  .brand_cont .card01 .image_area {
+    margin: 30px -30px -30px;
+    width: calc(100% + 60px);
+    border-radius: 0 0 20px 20px;
+  }
+}
+
+@media screen and (min-width: 1024px) {
+  .brand_cont .card01 .image_area {
+    margin: 40px -40px -40px;
+    width: calc(100% + 80px);
+  }
+}
+
 /* App Down Big Card */
 .app_down_card {
-  background: #E2221F;
+  background: #efe2d4; /* Much lighter skin color */
   color: white;
   border-radius: 40px;
-  padding: 60px;
+  padding: 60px 60px 0; /* Remove bottom padding for flush phone */
   margin-bottom: 100px;
+  overflow: hidden;
 }
 
 .app_downWrap {
   display: flex;
   gap: 60px;
-  align-items: center;
+  align-items: flex-end; /* Align bottom */
 }
 
+/* Enhancing image list to look like phone UI */
 .phone_ui {
   flex: 1;
+  position: relative;
+  height: 400px; /* Adjust based on image aspect */
+}
+
+.phone_ui .wrap {
+  width: 100%;
+  height: 100%;
+  position: relative;
 }
 
 .image_list {
-  display: flex;
-  gap: 20px;
+  position: relative;
+  width: 100%;
+  height: 100%;
   list-style: none;
+  padding: 0;
+  margin: 0;
 }
 
-.image_list img {
+.image_list li {
+  position: absolute;
+  bottom: 0;
+  transition: all 0.5s ease;
+}
+
+.image_list li img {
+  display: block;
   width: 100%;
-  border-radius: 20px;
+  max-width: 300px; /* Adjust size */
+  border-radius: 30px; /* Phone rounded corners */
+  box-shadow: 0 10px 30px rgba(0,0,0,0.2);
+}
+
+/* Stacking logic - adjusting positions to create a fanned out or layered look */
+.image_list li.an01 {
+  z-index: 3;
+  left: 50%;
+  transform: translateX(-50%);
+  bottom: 0px; /* Main phone */
+}
+
+.image_list li.an02 {
+  z-index: 2;
+  left: 50%;
+  transform: translateX(-80%) scale(0.9); /* Left behind */
+  bottom: 20px;
+  opacity: 0.8;
+}
+
+.image_list li.an03 {
+  z-index: 1;
+  left: 50%;
+  transform: translateX(-20%) scale(0.9); /* Right behind */
+  bottom: 20px;
+  opacity: 0.8;
 }
 
 .app_downWrap .cont {
   flex: 1.5;
+  padding-bottom: 60px;
+}
+
+.app_downWrap .tit {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  margin-bottom: 30px;
+  font-family: var(--font-BKR); /* Using BKR font */
+  line-height: 1.3;
+}
+
+.app_downWrap .tit .txt_red {
+  color: #E2221F;
+  font-size: 2.5rem; /* ~40px */
+  font-weight: 900;
+  text-align: left;
+}
+
+.app_downWrap .tit .txt_brown {
+  color: #502314;
+  font-size: 1.8rem; /* ~28px */
+  font-weight: 800;
+  text-align: left;
 }
 
 .benefit_list {
   display: flex;
-  gap: 20px;
-  margin: 30px 0;
+  flex-wrap: wrap;
+  gap: 10px;
+  margin-bottom: 40px;
 }
 
 .benefit_list strong {
-  border: 1px solid rgba(255,255,255,0.4);
-  padding: 8px 20px;
-  border-radius: 20px;
+  background: #502314;
+  color: white;
+  padding: 10px 20px;
+  border-radius: 10px;
+  font-size: 1.1rem;
+  font-weight: 700;
+  border: none; /* Remove previous border if any */
+}
+
+.btn_app.image_btn {
+  background: none;
+  border: none;
+  padding: 0;
+  width: 160px;
+  height: auto;
+  cursor: pointer;
+}
+
+.btn_app.image_btn img {
+  width: 100%;
+  height: auto;
+}
+
+.app_down {
+  display: flex;
+  align-items: flex-end;
+  gap: 20px;
+}
+
+.QR {
+  width: 100px;
+  height: 100px;
+  background: white;
+  border-radius: 10px;
+  padding: 5px;
+}
+
+.QR img {
+  width: 100%;
+  height: 100%;
 }
 
 .btn_area {
-  display: flex;
-  gap: 15px;
-  margin-top: 20px;
-}
-
-.btn_app {
-  background: white;
-  color: #E2221F;
-  padding: 15px 30px;
-  border-radius: 30px;
-  font-weight: 900;
+    display: flex;
+    gap: 10px;
 }
 
 @media screen and (max-width: 1024px) {
